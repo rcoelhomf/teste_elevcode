ceda31886d3382f3378ab327c8a2bd0ca5147b77
Renato <rcoelhomf@gmail.com>
2024-02-16 10:48:56 -0300

    initial commit

--- favorites/urls.py
+++ favorites/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import MovieSearchView
+from rest_framework_simplejwt import views as jwt_views
 
 
 urlpatterns = [
     path('movies/', MovieSearchView.as_view()),
+    path("login/", jwt_views.TokenObtainPairView.as_view()),
 ]
